126c6169ed47b6da7831a2b35ba169f7b42a00db
Thiago Carlos <thiagocarlossilvapereira@gmail.com>
2023-10-10 09:59:34 -0300

    Adicionando tela de boleto

--- serafin/src/navigation/Drawer.tsx
+++ serafin/src/navigation/Drawer.tsx
@@ -2,25 +2,27 @@ import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import Home from "../screens/HomeScreen";
 import Chat from "../screens/ChatScreen";
+import Boleto from "../screens/BoletoScreen";
 
 const Drawer = createDrawerNavigator();
 const screenOptions = {
-    tabBarStyle:{
-      backgroundColor:'#0000ff',
-      height:100,
-    },
-    tabBarItemStyle:{
-      backgroundColor:'#00ff00',
-      margin:5,
-      borderRadius:10,
-    }
-  };
+  tabBarStyle: {
+    backgroundColor: '#0000ff',
+    height: 100,
+  },
+  tabBarItemStyle: {
+    backgroundColor: '#00ff00',
+    margin: 5,
+    borderRadius: 10,
+  }
+};
 export default function MyDrawer() {
   return (
     <Drawer.Navigator>
 
-      <Drawer.Screen name="Início" component={Home}  />
+      <Drawer.Screen name="Início" component={Home} />
       <Drawer.Screen name="Fale com serafim" component={Chat} />
+      <Drawer.Screen name="Segunda via" component={Boleto} />
 
     </Drawer.Navigator>
   );
