5f4d851ec7399a2effdadf8e8eaf40ab60f09ab1
derleymad <wanderleymbf@gmail.com>
2023-12-02 17:04:47 -0300

    fixing darkmod all bugs

--- serafin/App.tsx
+++ serafin/App.tsx
@@ -1,20 +1,29 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { DarkTheme, NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './src/screens/splash/Splash';
 import Main from './src/screens/Main';
 
+
+import { useEffect } from 'react';
+import { Appearance } from 'react-native';
+
+
 const stack = createStackNavigator();
 
-export default function App(){
-  return(
-<NavigationContainer>
-  <stack.Navigator screenOptions={{headerShown:false}}>
-    <stack.Screen name="Splash" component={SplashScreen}/>
-    <stack.Screen name="Main" component={Main}/>
-  </stack.Navigator>
-<StatusBar barStyle="dark-content" backgroundColor="#ffffff"  />
-</NavigationContainer>
+export default function App() {
+  useEffect(() => Appearance.setColorScheme('light'),
+    [])
+  return (
+
+    <NavigationContainer>
+      <stack.Navigator screenOptions={{ headerShown: false }}>
+        <stack.Screen name="Splash" component={SplashScreen} />
+        <stack.Screen name="Main" component={Main} />
+      </stack.Navigator>
+      <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
+    </NavigationContainer>
+
   );
 }
